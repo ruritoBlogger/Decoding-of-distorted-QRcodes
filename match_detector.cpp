e5784e55e85e091f1b08d62702b513d2fc8bbb67
rurito0125 <rurito.blog@gmail.com>
2020-10-29 00:51:01 +0900

    fefactor: return以外で値を返すように修正

--- match_detector.cpp
+++ match_detector.cpp
@@ -6,20 +6,22 @@
  * BFMatcherを用いた特徴量一致を行う
  * @param teacherKeypoint 教師画像の特徴量
  * @param targetKeypoint 一致させたい画像の特徴量
+ * @param goodMatches 一致後の特徴量
  */
-void BFMatch(cv::Mat *teacherDescriptors, cv::Mat *targetDescriptors)
+void BFMatch(cv::Mat *teacherDescriptors,
+             cv::Mat *targetDescriptors,
+             std::vector<cv::DMatch> &goodMatches)
 {
     cv::BFMatcher matcher(cv::NORM_HAMMING);
     std::vector<std::vector<cv::DMatch> > nn_matches;
     matcher.knnMatch(*teacherDescriptors, *targetDescriptors, nn_matches, 2);
 
-    std::vector<cv::DMatch> good_matches;
     const float ratio_thresh = 0.5f;
     for (size_t i = 0; i < nn_matches.size(); i++)
     {
         if (nn_matches[i][0].distance < ratio_thresh * nn_matches[i][1].distance)
         {
-            good_matches.push_back(nn_matches[i][0]);
+            goodMatches.push_back(nn_matches[i][0]);
         }
     }
 }
