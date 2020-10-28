1825628f6736bbdddc108ac705874818217140e9
rurito0125 <rurito.blog@gmail.com>
2020-10-29 03:12:09 +0900

    feat: 抽出した特徴量をプロットする機能を実装

--- python/detector.py
+++ python/detector.py
@@ -1,8 +1,9 @@
 import cv2
 from cv2 import xfeatures2d
 import numpy as np
+from typing import List
 
-def star_algorithm( image: np.ndarray):
+def star_algorithm(image: np.ndarray) -> np.ndarray:
     """Star検出器を用いた特徴量抽出を行う
     :param: image 抽出したい画像
     :return: 特徴量
@@ -12,7 +13,7 @@ def star_algorithm( image: np.ndarray):
     return star.detect(image, None)
     # return star.detectAndCompute(image, None)
 
-def akaze_algorithm( image: np.ndarray):
+def akaze_algorithm(image: np.ndarray) -> any:
     """AKAZEを用いた特徴量抽出を行う
     :param: image 抽出したい画像
     :return: 特徴量, 特徴量ベクトル
@@ -20,3 +21,12 @@ def akaze_algorithm( image: np.ndarray):
 
     akaze = cv2.AKAZE_create()
     return akaze.detectAndCompute(image, None)
+
+def show_detector_point(image: np.ndarray, keypoint: List[any]) -> None:
+    """特徴量をプロットする
+    :param: image 特徴量を抽出した画像
+    :param: keypoint 特徴量
+    """
+    cv2.drawKeypoints(image, keypoint, image)
+    cv2.imshow("特徴量を描画した画像", image)
+    cv2.waitKey(0)
--- python/main.py
+++ python/main.py
@@ -2,13 +2,17 @@ from typing import List
 import numpy as np
 
 from read_data import read_data
-from detector import star_algorithm, akaze_algorithm
+from detector import star_algorithm, akaze_algorithm, show_detector_point
 
 def main():
     path: str = '../data'
     images: List[np.ndarray] = read_data(path)
     # keypoint, descriptor = star_algorithm(images[0])
     keypoint = star_algorithm(images[0])
+    show_detector_point(images[0], keypoint)
+    # keypoint, descriptor = akaze_algorithm(images[0])
+
+
 
 if __name__ == "__main__":
     main()
