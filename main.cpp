82e69be495853c07371169b6bbf554fcb380b368
rurito0125 <rurito.blog@gmail.com>
2020-10-29 00:52:17 +0900

    feat: 計算後の特徴量を用いて位置検出部分を実装

--- main.cpp
+++ main.cpp
@@ -34,12 +34,58 @@ int main()
     getFileNames(folderPath, fileNames);
     getFileImages(fileNames, images);
 
-    std::vector<cv::KeyPoint> keypoint;
-    cv::Mat descriptors;
-    // StarAlgorithm(images[0], keypoint, descriptors);
-    AkazeAlgorithm(images[0], keypoint, descriptors);
-    // ShowDetectorPoint(images[0], &keypoint);
+    for( auto image : images)
+    {
+        std::vector<cv::KeyPoint> keypoint;
+        cv::Mat descriptors;
+        // StarAlgorithm(images[0], keypoint, descriptors);
+        AkazeAlgorithm(image, keypoint, descriptors);
+        // ShowDetectorPoint(images[0], &keypoint);
 
-    // 教師画像の特徴量と比較する
-    BFMatch(&teacherDescrptors, &descriptors);
+        // 教師画像の特徴量と比較する
+        std::vector<cv::DMatch> good_matches;
+        BFMatch(&teacherDescrptors, &descriptors, good_matches);
+
+        cv::Mat matches_image;
+        cv::drawMatches(teacherImage, teacherDetectorKeypoint, image, keypoint, good_matches, matches_image, cv::Scalar::all(-1), cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+        std::vector<cv::Point2f> obj;
+        std::vector<cv::Point2f> scene;
+        std::cout << "good_matches.size()=" << good_matches.size() << std::endl;
+        for (int i = 0; i < good_matches.size(); i++)
+        {
+            //-- Get the keypoints from the good matches
+            obj.push_back(teacherDetectorKeypoint[good_matches[i].queryIdx].pt);
+            scene.push_back(keypoint[good_matches[i].trainIdx].pt);
+        }
+
+        // 見つけた場所に四角を描く
+        if (good_matches.size() > 2) {
+
+            cv::Mat H = cv::findHomography(obj, scene, cv::RANSAC);
+
+            if (!H.empty()) {
+                //-- Get the corners from the image_1 ( the object to be "detected" )
+                std::vector<cv::Point2f> obj_corners(4);
+                obj_corners[0] = cv::Point2f(0.0, 0.0);
+                obj_corners[1] = cv::Point2f((float)teacherImage.cols, 0.0);
+                obj_corners[2] = cv::Point2f((float)teacherImage.cols, (float)teacherImage.rows);
+                obj_corners[3] = cv::Point2f(0.0, (float)teacherImage.rows);
+
+                // □で囲む
+                std::vector<cv::Point2f> scene_corners(4);
+                cv::perspectiveTransform(obj_corners, scene_corners, H);
+
+                //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+                cv::line(matches_image, scene_corners[0] + cv::Point2f((float)teacherImage.cols, 0.0), scene_corners[1] + cv::Point2f((float)teacherImage.cols, 0.0), cv::Scalar(0, 255, 0), 4);
+                cv::line(matches_image, scene_corners[1] + cv::Point2f((float)teacherImage.cols, 0.0), scene_corners[2] + cv::Point2f((float)teacherImage.cols, 0.0), cv::Scalar(0, 255, 0), 4);
+                cv::line(matches_image, scene_corners[2] + cv::Point2f((float)teacherImage.cols, 0.0), scene_corners[3] + cv::Point2f((float)teacherImage.cols, 0.0), cv::Scalar(0, 255, 0), 4);
+                cv::line(matches_image, scene_corners[3] + cv::Point2f((float)teacherImage.cols, 0.0), scene_corners[0] + cv::Point2f((float)teacherImage.cols, 0.0), cv::Scalar(0, 255, 0), 4);
+
+                // cv::minAreaRectを使えばRotateRectを受け取ることができるのでそちらも検討
+                cv::imshow("Good Matches & Object detection", matches_image);
+                cv::waitKey(0);
+            }
+        }
+    }
 }
