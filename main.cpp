8758ef2023b3371fb7bf5f5dec1ceca164d04216
rurito0125 <rurito.blog@gmail.com>
2020-10-28 15:57:05 +0900

    feat: 取得出来た画像ファイル名を用いて画像を読み込む部分を実装

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <vector>
 #include "read_data.cpp"
+#include <opencv2/opencv.hpp>
 
 int main()
 {
     std::string folderPath = "./data";
     std::vector<std::string> fileNames;
     getFileNames(folderPath, fileNames);
+
+    cv::Mat img;
+    getFileImage(fileNames[0], img);
 }
