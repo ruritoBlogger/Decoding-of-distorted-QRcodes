0e4d5373498c0552bcdff790885a2506dcd6dfcf
rurito0125 <rurito.blog@gmail.com>
2020-10-28 23:50:01 +0900

    feat: 教師画像を用意して位置検出を行えるようにした

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 #include "read_data.cpp"
 #include "star_detector.cpp"
+#include "match_detector.cpp"
 
 /**
  * 複数の画像に対してStar検出器を用いて位置検出を行う
@@ -15,6 +16,17 @@ int main()
     // 画像の保存先ディレクトリ
     std::string folderPath = "./data";
 
+    // 教師画像と特徴量
+    std::string teacherImagePath = "./qrcode.png";
+    cv::Mat teacherImage;
+    std::vector<cv::KeyPoint> teacherDetectorKeypoint;
+    cv::Mat teacherDescrptors;
+    getFileImage(teacherImagePath, teacherImage);
+    StarAlgorithm(teacherImage, teacherDetectorKeypoint, teacherDescrptors);
+
+    // 画像を保存するならコメント外す
+    // SaveImageWithDetector(teacherImage, &teacherDetectorKeypoint, "teacher.jpg");
+
     // 保存先ディレクトリから画像を全て取得する
     std::vector<std::string> fileNames;
     std::vector<cv::Mat> images;
@@ -22,6 +34,10 @@ int main()
     getFileImages(fileNames, images);
 
     std::vector<cv::KeyPoint> keypoint;
-    StarAlgorithm(images[0], keypoint);
-    ShowDetectorPoint(images[0], &keypoint);
+    cv::Mat descriptors;
+    StarAlgorithm(images[0], keypoint, descriptors);
+    // ShowDetectorPoint(images[0], &keypoint);
+
+    // 教師画像の特徴量と比較する
+    BFMatch(&teacherDescrptors, &descriptors);
 }
